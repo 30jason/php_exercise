1ae03d35fc6cda1da345203a9d79f8bb4383312f
30jason <100329524+30jason@users.noreply.github.com>
2022-03-16 15:55:52 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,85 +1,40 @@
 <html>
 
+
 <head>
-<title>墾丁三日遊</title>
-<meta name='keyboard' content='墾丁, 墾丁三天兩夜, 墾丁三日遊' />
-<link rel='icon' href='https://m.media-amazon.com/images/I/61+iFsiGA+L.jpg' type='image/x-icon' />
-
-
+<title>HTML</title>
+<meta name="keywords" content="HTML, html, Html, Hello HTML" />
+<link rel="icon" href="/dashboard/ast.jpg" type="image/x-icon" />
 
 </head>
 
+
 <body>
-<body bgcolor='6BD4EB'>
-<h1><div align='center'>墾丁三天兩夜旅行</div></h1>
-<div align='center'><a href='https://www.fullon-hotels.com.tw/tw/' target='new'><img src='https://pix8.agoda.net/hotelImages/161/161736/161736_14061314210019840494.jpg?ca=2&ce=1&s=1024x768' width=30%></a>
-<a href='https://kenting.caesarpark.com.tw/' target='new'><img src='https://pix8.agoda.net/hotelImages/284/2842/2842_15030700280025886393.jpg?ca=3&ce=1&s=1024x768' width=30%></a>
+<?php
+echo "<body bgcolor='#FC5A37'>";
+?>
+<p style="font-style:italic;color;red;font-ize: 20px;">hello</P>
+<a href="https://www.nuk.edu.tw/" target="new">NUK</a></br>
+<div align='center'><a href="https://www.yunsanmotors.com/astonmartin/" target="new"><img src="/dashboard/ast.jpg" width="30%"></a>
 </div>
-<div align='center'>第一天:高雄集合出發->車城福安宮參拜->阿利海產->鹿境梅花鹿生態園區->福容大飯店CHECK IN->山下人家餐廳</br>
-                    <p>第二天:飯店Buffet午餐->龍磐公園->墾丁凱薩大飯店CHECK IN->小南灣->墾丁大街</br></p>
-                    <p>第三天:飯店Buffet早餐->恆春阿伯綠豆饌->屏東海生館->快樂賦歸</p>
-</div>
-
-<form action='' method=''>
-請輸入姓名: <input type='text' required placeholder='必填'></br>
-請輸入電子信箱: <input type='email' required placeholder='必填'></br>
-請輸入手機號碼: <input type='tel' required placeholder='必填'></br>
-請選擇性別: <input type='radio' name='gender' required>男性 <input type='radio' name='gender'>女性</br>
-請選擇你的飲食: <input type='checkbox' required>素食 <input type='checkbox'>葷食</br>
-請選擇你的T-shirt尺寸: <select required>
-                        <option>size</option>
-                        <option>S</option>
-                        <option>M</option>
-                        <option>L</option>
-                        <option>XL</option>
-                    </select></br> 
-請選擇你的T-shirt顏色: <input type='color' required></br>   
-請選擇你的生日: <input type='date' required></br>
-請輸入你要的旅遊券張數: <input type='number' required placeholder='必填'></br>
-<input type='submit'>
-
-</form>
 <div align='center'>
-男(單位:公分)
 <table border='1' width='30%'>
 <tr>
-<th></th><th>S</th><th>M</th><th>L</th><th>XL</th>    
+<th>Teams</th><th>Top Driver</th>
 </tr>
 <tr>
-<td><center>肩寬</td><td><center>44</td><td><center>45.5</td><td><center>47</td><td><center>48.5</td>      
+<td>Aston Martin</td><td>Sebastion Vettel</td>
 </tr>
 <tr>
-<td><center>胸寬</td><td><center>47.5</td><td><center>50.5</td><td><center>53.5</td><td><center>56.5</td>    
+<td>Mercedes</td><td>Lewis Hamilton</td>
 </tr>
 <tr>
-<td><center>袖長</td><td><center>20.5</td><td><center>21.5</td><td><center>22.5</td><td><center>23.5</td>
+<td>Ferrari</td><td>Charles Leclerc</td>
 </tr>
-<tr>
-<td><center>衣長</td><td><center>67.5</td><td><center>70.5</td><td><center>73.5</td><td><center>76.5</td>
-</tr>
-</table>
 
-<p>女(單位:公分)
-<table border='1' width='30%'> 
-<tr>
-<th></th><th>S</th><th>M</th><th>L</th><th>XL</th>    
-</tr>
-<tr>
-<td><center>肩寬</td><td><center>33</td><td><center>34</td><td><center>35</td><td><center>37</td>      
-</tr>
-<tr>
-<td><center>胸寬</td><td><center>34</td><td><center>37.5</td><td><center>39</td><td><center>43.5</td>    
-</tr>
-<tr>
-<td><center>袖長</td><td><center>22</td><td><center>23.5</td><td><center>23.5</td><td><center>24</td>
-</tr>
-<tr>
-<td><center>衣長</td><td><center>58</td><td><center>60</td><td><center>62</td><td><center>64</td>
-</tr>
-</table>
-</p>
-</div>
 
+</table>
 </body>
+
 
 </html>
