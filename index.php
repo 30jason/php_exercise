43bc76e95555cbfffc541573e14d4be51a37955d
30jason <100329524+30jason@users.noreply.github.com>
2022-03-16 15:56:23 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,11 @@
-<html>
-
-
-<head>
-<title>HTML</title>
-<meta name="keywords" content="HTML, html, Html, Hello HTML" />
-<link rel="icon" href="/dashboard/ast.jpg" type="image/x-icon" />
-
-</head>
-
-
-<body>
 <?php
-echo "<body bgcolor='#FC5A37'>";
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
 ?>
-<p style="font-style:italic;color;red;font-ize: 20px;">hello</P>
-<a href="https://www.nuk.edu.tw/" target="new">NUK</a></br>
-<div align='center'><a href="https://www.yunsanmotors.com/astonmartin/" target="new"><img src="/dashboard/ast.jpg" width="30%"></a>
-</div>
-<div align='center'>
-<table border='1' width='30%'>
-<tr>
-<th>Teams</th><th>Top Driver</th>
-</tr>
-<tr>
-<td>Aston Martin</td><td>Sebastion Vettel</td>
-</tr>
-<tr>
-<td>Mercedes</td><td>Lewis Hamilton</td>
-</tr>
-<tr>
-<td>Ferrari</td><td>Charles Leclerc</td>
-</tr>
-
-
-</table>
-</body>
-
-
-</html>
+Something is wrong with the XAMPP installation :-(
